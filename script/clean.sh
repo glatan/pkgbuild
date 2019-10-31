210c0d10bf720082d39f3d9bceaa885c082711a6
glatan <glatan.edu@gmail.com>
2019-10-31 17:10:37 +0900

    support AUR/pkg cache

--- script/clean.sh
+++ script/clean.sh
@@ -2,12 +2,14 @@
 
 set -u
 
-declare DRYRUN='echo'                                           # echo(default): dry-run, rm -r: remove
-declare -r ALL_CACHE='*/*/* */*/.*'                             # all makepkg's cache
-declare -r BUILD_CACHE='*/*/pkg */*/src */*/.* */*/*.pkg.* */AUR/*/pkg */AUR/*/src */AUR/*/.* */AUR/*/*.pkg.*'     # makepkg's build cache
-declare REMOVE_MODE="${BUILD_CACHE}"                            # meta val
-declare -a TARGET_PACKAGE                                       # remove target package
-declare ALL_PACKAGES='0'                                        # 0: all packages, 1: choosed package
+# makepkg's all cache
+declare -r ALL_CACHE='*/*/* */*/.* */AUR/*/* */AUR/*/.*'
+# makepkg's build cache
+declare -r BUILD_CACHE='*/*/pkg */*/src */*/.* */*/*.pkg.* */AUR/*/pkg */AUR/*/src */AUR/*/.* */AUR/*/*.pkg.*'
+declare DRYRUN='echo'                    # echo(default): dry-run, rm -r: remove
+declare REMOVE_MODE="${BUILD_CACHE}"     # meta val
+declare -a TARGET_PACKAGE                # remove target package
+declare ALL_PACKAGES='0'                 # 0: all packages, 1: choosed package
 
 while (( $# > 0 )); do
     case "${1}" in

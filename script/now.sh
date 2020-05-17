11372959852efbfc9d71aff72e1ecb3983e930d6
glatan <glatan.edu@gmail.com>
2020-05-18 01:21:07 +0900

    clone in build process

--- script/now.sh
+++ script/now.sh
@@ -4,4 +4,7 @@ yum update -y
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.rpm.sh | bash
 yum install -y git-lfs
 
+git clone https://gitlab.com/glatan/pkgbuild.git
+cd pkgbuild || exit
 git lfs pull
+cp -rf release ../release

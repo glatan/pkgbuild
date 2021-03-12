13e19860df26b55e5f210d24430e8d12e783a4ba
glatan <glatan@protonmail.com>
2021-03-13 01:57:50 +0900

    Add linux-ripemd

--- script/build.sh
+++ script/build.sh
@@ -10,10 +10,13 @@ pacman -Syy
 
 chown -R makepkg ./
 
-if su makepkg -c 'makepkg -C -f -s --noconfirm'; then
+if su makepkg -c 'makepkg -f -s --noconfirm'; then
     source PKGBUILD
-    chown root:root "${1}-${pkgver}-${pkgrel}-${arch}.pkg.tar.zst"
-    mv "${1}-${pkgver}-${pkgrel}-${arch}.pkg.tar.zst" ../../release
+    # "${1}-${pkgver}-${pkgrel}-${arch}.pkg.tar.zst"だと linux,
+    # linux-headers, linux-docsみたいに複数パッケージを一括ビルド
+    # する場合に対応できないためこうなっている。
+    chown root:root ./*"${pkgver}-${pkgrel}-${arch}.pkg.tar.zst"
+    mv ./*"${pkgver}-${pkgrel}-${arch}.pkg.tar.zst" ../../release
 fi
 
 chown -R root ./
